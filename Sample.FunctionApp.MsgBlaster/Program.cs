90288064eae59f8d7571afc0790dcf80bc5a7695
Animesh Agarwal <58473075+animat089@users.noreply.github.com>
2022-10-22 14:18:52 +0530

    Uploaded Code Coverage

--- Sample.FunctionApp.MsgBlaster/Program.cs
+++ Sample.FunctionApp.MsgBlaster/Program.cs
@@ -1,35 +1,50 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Microsoft.Extensions.Configuration;
+using System.Diagnostics.CodeAnalysis;
 
-var configuration = new ConfigurationBuilder()
-    .AddUserSecrets<Program>()
-    .Build();
-var connectionString = configuration["QueueConnectionString"].ToString();
-var queueName = configuration["QueueName"].ToString();
-var clientOptions = new ServiceBusClientOptions()
+namespace Sample.FunctionApp.MsgBlaster;
+
+public class Program
 {
-    TransportType = ServiceBusTransportType.AmqpTcp
-};
-
-var client = new ServiceBusClient(connectionString);
-var sender = client.CreateSender(queueName);
-
-try
-{
-    Console.WriteLine("Started");
-    Parallel.For(0, 2000, new ParallelOptions() { MaxDegreeOfParallelism = 8 }, i =>
+    [ExcludeFromCodeCoverage]
+    public static void Main()
     {
-        var msg = new ServiceBusMessage(Convert.ToString(i));
-        sender.SendMessageAsync(msg).GetAwaiter().GetResult();
-    });
-    Console.WriteLine("Completed");
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex);
-}
-finally
-{
-    sender.DisposeAsync().GetAwaiter().GetResult();
-    client.DisposeAsync().GetAwaiter().GetResult();
+        // Create Configuration
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+        // Read from User Secrets
+        var connectionString = configuration["QueueConnectionString"].ToString();
+        var queueName = configuration["QueueName"].ToString();
+
+        // Create client and connections
+        var clientOptions = new ServiceBusClientOptions()
+        {
+            TransportType = ServiceBusTransportType.AmqpTcp
+        };
+        var client = new ServiceBusClient(connectionString);
+        var sender = client.CreateSender(queueName);
+
+        // Send messages and exit
+        try
+        {
+            Console.WriteLine("Started");
+            Parallel.For(0, 2000, new ParallelOptions() { MaxDegreeOfParallelism = 8 }, i =>
+            {
+                var msg = new ServiceBusMessage(Convert.ToString(i));
+                sender.SendMessageAsync(msg).GetAwaiter().GetResult();
+            });
+            Console.WriteLine("Completed");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            sender.DisposeAsync().GetAwaiter().GetResult();
+            client.DisposeAsync().GetAwaiter().GetResult();
+        }
+    }
 }
